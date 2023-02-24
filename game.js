ae50fb9a73e55cb3baf50add5c26b8d1312d6ee6
anthonyphasay <72152681+anthonyphasay@users.noreply.github.com>
2023-02-23 17:02:28 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -5,13 +5,7 @@ var userClickedPattern =[];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
-    if (!started) {
-        $("#level-title").text("Level " + level);
-        nextSequence();
-        started = true;
-    }
-}).on('click', function() {
+$(document).keydown(function() {
     if (!started) {
         $("#level-title").text("Level " + level);
         nextSequence();
